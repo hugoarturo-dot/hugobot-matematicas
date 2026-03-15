fdf4dfe933680c3c08845e4ac4c201e49d260bbb
hugoarturo-dot <hugovandyk@gmail.com>
2026-03-15 00:06:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ if st.sidebar.button("🔄 Nueva Clase / Reiniciar Chat"):
 
 if "chat_session" not in st.session_state:
     st.session_state.chat_session = st.session_state.cliente_ia.chats.create(
-        model="gemini-2.0-flash", # Cambiamos al modelo de máxima estabilidad
+        model="gemini-1.5-flash-8b", # Cambiamos al modelo de máxima estabilidad
         config=types.GenerateContentConfig(
             system_instruction=instrucciones_sistema,
             temperature=0.2
